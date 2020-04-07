f3b836c56e463584f3f00582649fdb69c61b400a
morganmark <32056855+morganmark@users.noreply.github.com>
2020-04-07 16:51:04 +0800

    0407

---
+++ Unity_ML_Football/Assets/Script/Ball.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public static bool complete;
+    private void OnTriggerEnter(Collider other) //soccer enter door,other = 碰到物件碰撞資訊
+    {
+        if(other.name == "進球感應區"){
+            complete = true;
+        }
+    }
+}
---
+++ Unity_ML_Football/Assets/Script/Robot.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using MLAgents;
+using MLAgents.Sensors;
+
+public class Robot : Agent
+{
+    [Header("speed"), Range(1, 50)]
+    public float speed = 10;
+    private Rigidbody rigRobot;
+    private Rigidbody rigBall;
+
+    private void Start()
+    {
+        rigRobot = GetComponent<Rigidbody>();
+        rigBall = GameObject.Find("足球").GetComponent<Rigidbody>();
+    }
+    public override void OnEpisodeBegin()// the game restart environment
+    {
+        rigRobot.velocity = Vector3.zero;
+        rigRobot.angularVelocity = Vector3.zero;
+        rigBall.velocity = Vector3.zero;
+        rigBall.angularVelocity = Vector3.zero;
+
+        Vector3 posRobot = new Vector3(Random.Range(-3.7f, 3.7f), 0.1f , Random.Range(-2f,0f));
+        transform.position = posRobot;
+
+        Vector3 posBall = new Vector3(Random.Range(-3.7f, 3.7f), 0.1f, Random.Range(1f, 2f));
+        rigBall.position = posBall;
+
+        Ball.complete = false;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(rigBall.position);
+        sensor.AddObservation(rigRobot.velocity.x);
+        sensor.AddObservation(rigRobot.velocity.y);
+
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        Vector3 control = Vector3.zero;
+        control.x = vectorAction[0];
+        control.z = vectorAction[1];
+        rigRobot.AddForce(control * speed);
+
+        if (Ball.complete)
+        {
+            SetReward(1);
+            EndEpisode();
+        }
+
+        if (transform.position.y < 0 || rigBall.position.y < 0)
+        {
+            SetReward(-1);
+            EndEpisode();
+        }
+    }
+
+    public override float[] Heuristic()//測試
+    {
+        var action = new float[2];
+        action[0] = Input.GetAxis("Horizontal");
+        action[1] = Input.GetAxis("Vertical");
+        return action;
+    }
+}
